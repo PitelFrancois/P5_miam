7f678f08743bc21cfe2b15ffbb8cdd62a8d2b212
Pitel Francois <francoispitel87@gmail.com>
2021-08-09 15:01:05 +0200

    Search

---
+++ Public/Frontend/js/Search.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#search-input').keyup(function(){
+        $('#resultSearch').html('');
+        let recipe = $(this).val();    
+        if (recipe != ""){
+            $.ajax({
+                type: 'GET',
+                url: '/search.php',
+                data: 'recipe=' + encodeURIComponent(recipe),
+                success: function(data){
+                    if (data != ""){
+                        $('#resultSearch').append(data);
+                    } else {
+                        document.getElementById('resultSearch').innerHTML = "Recette introuvable."
+                    };
+                }
+            });
+        };
+    });
+});
---
+++ Search.php
@@ -0,0 +1,13 @@
+<?php
+    use Francois\Models\Database;
+    require_once "./Models/Database.php";
+    if (isset($_GET['recipe'])){
+        $recipe = $_GET['recipe'];
+        $db = new Database;
+        $req = $db->request("SELECT * FROM miamrecipe WHERE title LIKE ? LIMIT 10",["%$recipe%"])->fetchAll();  
+        foreach ($req as $r){
+        ?>
+        <a href="/recipe/read/<?= $r['id'];?>"><h2 style="color:white" ><?= $r['title'];?></h2></a>
+        <?php    
+        }
+    }
