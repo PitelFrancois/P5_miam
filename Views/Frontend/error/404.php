8ba8d31eaaf86cec2d534a90d7bb93901a337a84
Pitel Francois <francoispitel87@gmail.com>
2021-08-09 11:10:48 +0200

    View Error

---
+++ Views/Frontend/error/404.php
@@ -0,0 +1,10 @@
+<?php $this->title = "404"; ?>
+
+<section class="section-404">
+    <div class="text-404">
+        <h1>404</h1>
+        <h3>Oups! Cette page est introuvable!</h3>
+        <p>Désolé, la page que vous recherchez n'existe pas, a été supprimée ou son nom a changé</p>
+        <a href="/"><h3>Revenir sur la page d'accueil</h3></a>
+    </div>
+</section>
